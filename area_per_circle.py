4a67b37f73cfcb026c5eb07dfc18be1eb7307346
Marli Peters <marley.peters@stu.ocsb.ca>
2023-09-20 18:02:39 +0000

    Added area_per_circle.py

---
+++ area_per_circle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created By: Marli Peters
+
+# Date: Sep. 20, 2023
+
+# This program calculates the circumference and area of a circle.
+
+import math
+
+print("If a circle has a radius of:")
+print("8")
+print("")
+rad = 8
+circ = math.pi * 2 * rad
+area = math.pi * rad**2
+print("Circumference is", circ)
+print("Area is", area)
---
+++ math_test_case.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+
+print("If a rectangle has the dimensions:")
+print("6cm x 4cm")
+print("")
+print("Area is {}cm^2".format(6 * 4))
+print("")
+print("If a circle has a radius of:")
+print("8")
+print("")
+rad = 8
+circ = math.pi * 2 * rad
+area = math.pi * rad**2
+print("Circumference is", circ, area)
