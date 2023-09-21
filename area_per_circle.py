3574c468ff72cb6e0aa2f7907aa8a6f758bfa336
Marli Peters <marley.peters@stu.ocsb.ca>
2023-09-21 16:57:16 +0000

    Updated area_per_circ.py

--- area_per_circle.py
+++ area_per_circle.py
@@ -8,11 +8,20 @@
 
 import math
 
+# Prints the circumference of 8.
 print("If a circle has a radius of:")
 print("8")
 print("")
 rad = 8
-circ = math.pi * 2 * rad
-area = math.pi * rad**2
-print("Circumference is", circ)
-print("Area is", area)
+circ = math.pi * float(2) * rad
+area = math.pi * rad**float(2)
+print("Circumference is {:.2f}".format(circ))
+# Prints the area of 2.4.
+print("")
+print("If a circle has a radius of:")
+print("2.4")
+print("")
+rad = 2.4
+circ = math.pi * float(2) * rad
+area = math.pi * rad** float(2)
+print("Area is {:.2f}".format(area))
